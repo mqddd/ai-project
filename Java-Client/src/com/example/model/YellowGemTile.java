fdaa9433a98f008f32a11491315cac187f7dae84
m.dehghan <meghdadd78@gmail.com>
2021-12-23 12:48:14 +0330

    yellow gem minimum required score fixed

--- Java-Client/src/com/example/model/YellowGemTile.java
+++ Java-Client/src/com/example/model/YellowGemTile.java
@@ -2,7 +2,7 @@ package com.example.model;
 
 public enum YellowGemTile {
     REWARD(10),
-    REQUIRED_SCORE(0),
+    REQUIRED_SCORE(Integer.MIN_VALUE),
     MAXIMUM_ACHIEVABLE_TIMES(15);
 
     private final int value;
